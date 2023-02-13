a123799e5267f5b29d06f3fe93301eb5dc00f950
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-02-13 17:57:26 +0530

    Update.

---
+++ APIs for Beginners 2023 - How to use an API (Full Course, Tutorial) - Craig Dennis/API.py
@@ -0,0 +1,50 @@
+#APIs for Beginners 2023 - How to use an API (Full Course / Tutorial) - Craig Dennis. 
+#https://www.youtube.com/watch?v=WXsD0ZgxjRw&t=2s
+
+# This is the notes for the tutorial. 
+
+'''
+API - Application Programming Interface. Web APIs. Interface - GIU(Graphical User Interface). 
+Interface - The details are abstracted to the user, but the user is still in control of what has been determined that the user can handle. 
+Interfaces evolve over time. 
+GUI - Graphical User Interface. Buttons are an interface. They provide the developer the means of controlling its interactions while... 
+...abstracting way how that actually works, implementation wise.
+What interfaces have in common - They all define ways for us to interact or communicate with an object, whether that object be physical...
+...or software, and as a user of the interface, we don't need to understand the implementation, we don't need to know how it works.
+We just need to know what we've been allowed to change or see, interfaces abstract away the implementation.
+
+UI - User Interface. 
+API is a contract of sorts. It defines how it is expected to be used and it defines what you can expect to receive by using it. 
+API - Web Based API. Mostly. Frameworks provide an API that allow you to extend what has been provided to make it your own.
+
+Remote APIs - Computational power. Insanely fast. Remote APIs are designed to interact through a communications network. 
+By remote, we mean that the resources being manipulated by the API are somewhere outside the computer making the request.
+REST - Representational State Transfer. GraphQL. APIs written in REST style. 
+
+Browser - Web Client. Connect to a server. Location in address bar. URL(Universal Resource Locator). 
+Superset term - URI (Universal Resource Identifier). URL has a scheme portion - HTTP. HTTP - Hypertext Transfer Protocol. 
+Protocol - Lower level of API. HTTP verb - GET verb - used to clarify that this request will only receive data. 
+No changes. Server receives request that was transferred from the client. 
+Server does its work. It generates resulting webpage and sends back a response to the client (browser). 
+Hyper text is text that can link to other texts. 
+HTML, anchor tags. 
+
+HTTP as it was originally designed is a stateless protocol. 
+Headers. Both requests and responses have headers. 
+Caching. Status code. List of HTTP header fields. List of HTTP status codes. 
+
+Guiding Architectural Constraints required for an API to be considered RESTful -
+Client-Server Architecture, Statelessness, Cache-ability, Layered System, Code on Demand, Uniform Interface.
+Resource - R in URL. Reference an object. Describe just about anything that we build. 
+Resources and collections. CRUD (Creating, Reading, Updating, Deleting).   
+Read request. GET request to URI. JSON (Javascript Object Notation).
+JSON provides a great way to structure and nest your data. 
+JSON string into a native object. Other HTTP verbs are available. 
+GET, POST, PUT, PATCH, DELETE.
+Mashup. Many APIs are available. API calls. 
+
+Spotify For-Developers. SDK(Software Development Kit).
+API provides a set of endpoints, each with its own unique path. 
+
+''' 
+
