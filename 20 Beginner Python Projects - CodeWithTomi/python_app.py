6736ad7989c2e44dd7eaa4e7031c8dc9f92baca3
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-02-17 19:35:38 +0530

    Update.

---
+++ 20 Beginner Python Projects - CodeWithTomi/python_app.py
@@ -0,0 +1,31 @@
+#https://www.youtube.com/watch?v=pdy3nh1tn6I&ab_channel=freeCodeCamp.org
+
+#Email Sender. 
+
+'''
+Go over to our gmail account and setup 2 factor authentication.
+Generate app password.
+Create a function to send the mail.
+'''
+from email.message import EmailMessage
+from app2 import password
+import ssl
+import smtplib
+email_sender = 'anirudhpreeth@gmail.com'
+email_password = password
+email_receiver = 'joyoma7243@iucake.com'
+subject = "Don't forget to subscribe."
+body = """
+When you watch a video, please hit subscribe."""
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
+    
+
+    
